01de6fcfd12e086eb851a4a932402e6add5b6a29
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-08-28 14:58:10 +0530

    All known route handel

--- app.mjs
+++ app.mjs
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.all("*", (req, res) => {
+  res.status(404).json({
+    message: "route not found",
+  });
+});
+
 export default app;
