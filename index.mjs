13c4c810262a9243eb0de6f5f1bc867802cd00a0
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-08-28 15:12:05 +0530

    mongoose installing and database connected successfully

--- index.mjs
+++ index.mjs
@@ -1,4 +1,12 @@
 import app from "./app.mjs";
+import mongoose from "mongoose";
+
+const dataBaseUrl =
+  "mongodb+srv://School-management:340384792347@cluster0.4jxf50a.mongodb.net/shani";
+mongoose
+  .connect(dataBaseUrl, {})
+  .then(() => console.log("Connected successfully to server"))
+  .catch((error) => console.log(error));
 
 const port = 5000;
 app.listen(port, () => {
